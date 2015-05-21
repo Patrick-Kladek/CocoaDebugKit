739327f32c9716b42ed255e81b7d23cb0c95d96b
Patrick Kladek <patrick@Patricks-Mac-mini.local>
2015-05-21 18:57:58 +0200

    pkDebugFramework created pkDebugView implemented.

---
+++ Project/Test Application/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Test Application
+//
+//  Created by Patrick Kladek on 21.05.15.
+//  Copyright (c) 2015 Patrick Kladek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "pkDebugFramework.h"
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
