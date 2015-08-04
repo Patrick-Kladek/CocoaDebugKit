18b21ec3a85cbdf6bfa1be781f3dce3bed218a8c
Patrick Kladek <patrick@Patricks-Mac-mini-2.local>
2015-08-04 20:56:45 +0200

    get all properties from class and displays them automatically TODO optional: display also object Type (NSString, NSDate, ...) highlight them light grey or in Syntax Color. highlight with options. load plist with options (should look like xcode highlights). ...

---
+++ Project/Test Application/pkTestObject.h
@@ -0,0 +1,27 @@
+//
+//  pkTestObject.h
+//  pkDebugFramework
+//
+//  Created by Patrick Kladek on 04.08.15.
+//  Copyright (c) 2015 Patrick Kladek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface pkTestObject : NSObject
+
+@property (nonatomic) NSDate *date;
+@property (nonatomic) NSImage *image;
+@property (nonatomic) id prop;
+
+@property (nonatomic) NSString *name;
+@property (nonatomic) NSString *test;
+
+@property (nonatomic) BOOL check;
+@property (nonatomic) unsigned char ccheck;
+@property (nonatomic) int inum;
+@property (nonatomic) long lnum;
+
+- (id)debugQuickLookObject;
+
+@end
