4074f1f3d2aa2095cf65d9cb65a25b68f5be6228
Patrick-Kladek <>
2016-04-19 15:53:44 +0200

    pkDebugDescription added should create description string from properties

---
+++ Project/pkDebugFramework/pkDebugDescription.m
@@ -0,0 +1,13 @@
+//
+//  pkDebugDescription.m
+//  pkDebugFramework
+//
+//  Created by Patrick Kladek on 19.04.16.
+//  Copyright (c) 2016 Patrick Kladek. All rights reserved.
+//
+
+#import "pkDebugDescription.h"
+
+@implementation pkDebugDescription
+
+@end
