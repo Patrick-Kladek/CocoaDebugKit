27857230be043dff2b6e9afa032c1ddf697c9639
Patrick-Kladek <>
2016-04-25 22:10:31 +0200

    added trace for subclasses

--- Project/pkDebugFramework/pkPropertyEnumerator.h
+++ Project/pkDebugFramework/pkPropertyEnumerator.h
@@ -11,7 +11,12 @@
 @interface pkPropertyEnumerator : NSObject
 
 //- (void)enumerateProperties:(NSObject *)obj allowed:(NSString *)allowed;
-- (void)enumerateProperties:(NSObject *)obj allowed:(NSString *)allowed block:(void (^)(NSString *type, NSString *name))callbackBlock;
+//- (void)enumerateProperties:(NSObject *)obj allowed:(NSString *)allowed block:(void (^)(NSString *type, NSString *name))callbackBlock;
+
+- (void)enumerateProperties:(Class)objectClass allowed:(NSString *)allowed block:(void (^)(NSString *type, NSString *name))callbackBlock;
+
+
+
 - (NSString *)propertyTypeFromName:(NSString *)name object:(NSObject *)obj;
 
 @end
