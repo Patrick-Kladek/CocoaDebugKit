e2897ecb8718f89e83bedabb5e2b345d74a663b3
Patrick Kladek <patrick@Patricks-MacBook-Pro.local>
2017-05-25 19:31:53 +0200

    add iOS support

---
+++ iOS/CocoaTouchDebugKit/CocoaTouchDebugKit.h
@@ -0,0 +1,23 @@
+//
+//  CocoaTouchDebugKit.h
+//  CocoaTouchDebugKit
+//
+//  Created by Patrick Kladek on 21.05.17.
+//  Copyright (c) 2017 Patrick Kladek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaTouchDebugKit.
+FOUNDATION_EXPORT double CocoaTouchDebugKitVersionNumber;
+
+//! Project version string for CocoaTouchDebugKit.
+FOUNDATION_EXPORT const unsigned char CocoaTouchDebugKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaTouchDebugKit/PublicHeader.h>
+
+#import <CocoaTouchDebugKit/CocoaDebugView.h>
+#import <CocoaTouchDebugKit/CocoaDebugSettings.h>
+#import <CocoaTouchDebugKit/CocoaDebugDescription.h>
+#import <CocoaTouchDebugKit/CocoaPropertyEnumerator.h>
+
